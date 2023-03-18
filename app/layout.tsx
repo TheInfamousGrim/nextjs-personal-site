2acadf7d3f5ae2fcad78bf0ef880faab2f0870f8
TheInfamousGrim <finchergeorge1@gmail.com>
2023-03-18 17:16:39 +0000

    Style 💄: Style borders for page container.
    Styled the side and footer borders aquamarine and bright pink colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({
         <StarsCanvas />
         <div className="fixed inset-0 flex justify-center sm:px-8">
           <div className="flex w-full max-w-7xl lg:px-8">
-            <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20"></div>
+            <div className="w-full bg-white ring-1 ring-aquamarine dark:bg-zinc-900"></div>
           </div>
         </div>
         <div className="relative">
